7da7e74dae96163c175814079377a348bcf2c029
Ivan Novakov <ivan.novakov@debug.cz>
2014-04-17 14:11:44 +0200

    the user entity

--- src/User/User.php
+++ src/User/User.php
@@ -2,6 +2,11 @@
 
 namespace Udb\Domain\User;
 
+use Udb\Domain\Entity\Collection\RoomCollection;
+use Udb\Domain\Entity\Collection\LabelledUrlCollection;
+use Udb\Domain\Entity\Collection\EmailAddressCollection;
+use Udb\Domain\Entity\Collection\PhoneCollection;
+
 
 /**
  * The user entity.
@@ -49,25 +54,40 @@ class User
      */
     protected $status;
 
-    protected $workPhone;
-
-    protected $mobilePhone;
+    /**
+     * @var PhoneCollection
+     */
+    protected $workPhones;
 
     /**
-     * @var string
+     * @var PhoneCollection
      */
-    protected $room;
+    protected $mobilePhones;
+
+    /**
+     * @var RoomCollection
+     */
+    protected $rooms;
 
     /**
      * @var string
      */
     protected $department;
 
-    protected $homepage;
+    /**
+     * @var LabelledUrlCollection
+     */
+    protected $urls;
 
-    protected $emailForwarding;
+    /**
+     * @var EmailAddressCollection
+     */
+    protected $emailForwardings;
 
-    protected $emailAlternative;
+    /**
+     * @var EmailAddressCollection
+     */
+    protected $emailAlternatives;
 
 
     /**
@@ -103,5 +123,303 @@ class User
     public function setUsername($username)
     {
         $this->username = $username;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+
+    /**
+     * @param string $fullName
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getEmployeeType()
+    {
+        return $this->employeeType;
+    }
+
+
+    /**
+     * @param string $employeeType
+     */
+    public function setEmployeeType($employeeType)
+    {
+        $this->employeeType = $employeeType;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+
+    /**
+     * @param string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+
+    /**
+     * @return PhoneCollection
+     */
+    public function getWorkPhones()
+    {
+        if (! $this->workPhones instanceof PhoneCollection) {
+            $this->workPhones = new PhoneCollection();
+        }
+        
+        return $this->workPhones;
+    }
+
+
+    /**
+     * @param PhoneCollection|array $workPhones
+     */
+    public function setWorkPhones($workPhones)
+    {
+        if (! $workPhones instanceof PhoneCollection) {
+            $this->checkCollectionData($workPhones, 'PhoneCollection');
+            $workPhones = new PhoneCollection($workPhones);
+        }
+        
+        $this->workPhones = $workPhones;
+    }
+
+
+    /**
+     * @return PhoneCollection
+     */
+    public function getMobilePhones()
+    {
+        if (! $this->mobilePhones instanceof PhoneCollection) {
+            $this->mobilePhones = new PhoneCollection();
+        }
+        
+        return $this->mobilePhones;
+    }
+
+
+    /**
+     * @param PhoneCollection|array $mobilePhones
+     */
+    public function setMobilePhones($mobilePhones)
+    {
+        if (! $mobilePhones instanceof PhoneCollection) {
+            $this->checkCollectionData($mobilePhones, 'PhoneCollection');
+            $mobilePhones = new PhoneCollection($mobilePhones);
+        }
+        
+        $this->mobilePhones = $mobilePhones;
+    }
+
+
+    /**
+     * @return RoomCollection
+     */
+    public function getRooms()
+    {
+        if (! $this->rooms instanceof RoomCollection) {
+            $this->rooms = new RoomCollection();
+        }
+        
+        return $this->rooms;
+    }
+
+
+    /**
+     * @param RoomCollection|array $room
+     */
+    public function setRooms($rooms)
+    {
+        if (! $rooms instanceof RoomCollection) {
+            $this->checkCollectionData($rooms, 'RoomCollection');
+            $rooms = new RoomCollection($rooms);
+        }
+        
+        $this->rooms = $rooms;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+
+
+    /**
+     * @param string $department
+     */
+    public function setDepartment($department)
+    {
+        $this->department = $department;
+    }
+
+
+    /**
+     * @return LabelledUrlCollection
+     */
+    public function getUrls()
+    {
+        if (! $this->urls instanceof LabelledUrlCollection) {
+            $this->urls = new LabelledUrlCollection();
+        }
+        
+        return $this->urls;
+    }
+
+
+    /**
+     * @param LabelledUrlCollection $urls
+     */
+    public function setUrls(LabelledUrlCollection $urls)
+    {
+        $this->urls = $urls;
+    }
+
+
+    /**
+     * @return EmailAddressCollection
+     */
+    public function getEmailForwardings()
+    {
+        if (! $this->emailForwardings instanceof EmailAddressCollection) {
+            $this->emailForwardings = new EmailAddressCollection();
+        }
+        
+        return $this->emailForwardings;
+    }
+
+
+    /**
+     * @param EmailAddressCollection|array $emailForwardings
+     */
+    public function setEmailForwardings($emailForwardings)
+    {
+        if (! $emailForwardings instanceof EmailAddressCollection) {
+            $this->checkCollectionData($emailForwardings, 'EmailAddressCollection');
+            $emailForwardings = new EmailAddressCollection($emailForwardings);
+        }
+        
+        $this->emailForwardings = $emailForwardings;
+    }
+
+
+    /**
+     * @return EmailAddressCollection
+     */
+    public function getEmailAlternatives()
+    {
+        if (! $this->emailAlternatives instanceof EmailAddressCollection) {
+            $this->emailAlternatives = new EmailAddressCollection();
+        }
+        
+        return $this->emailAlternatives;
+    }
+
+
+    /**
+     * @param EmailAddressCollection|array $emailAlternatives
+     */
+    public function setEmailAlternatives($emailAlternatives)
+    {
+        if (! $emailAlternatives instanceof EmailAddressCollection) {
+            $this->checkCollectionData($emailAlternatives, 'EmailAddressCollection');
+            $emailAlternatives = new EmailAddressCollection($emailAlternatives);
+        }
+        
+        $this->emailAlternatives = $emailAlternatives;
+    }
+
+
+    /**
+     * Checks if the provided data are suitable for a collection.
+     * 
+     * @param mixed $value
+     * @param string $collectionName
+     * @throws Exception\InvalidValueException
+     */
+    protected function checkCollectionData($value, $collectionName)
+    {
+        if (! is_array($value)) {
+            throw new Exception\InvalidValueException(sprintf("Expecting %s or array", $collectionName));
+        }
     }
 }
--- tests/unit/User/UserTest.php
+++ tests/unit/User/UserTest.php
@@ -2,6 +2,10 @@
 
 namespace UdbTest\Domain\User;
 
+use Udb\Domain\Entity\Collection\RoomCollection;
+use Udb\Domain\Entity\Collection\EmailAddressCollection;
+use Udb\Domain\Entity\Collection\LabelledUrlCollection;
+use Udb\Domain\Entity\Collection\PhoneCollection;
 use Udb\Domain\User\User;
 
 
@@ -13,12 +17,188 @@ class UserTest extends \PHPUnit_Framework_TestCase
     {
         $id = 123;
         $username = 'testuser';
+        $firstName = 'Test';
+        $lastName = 'User';
+        $fullName = 'Dr. Test User';
+        $email = 'test.user@example.org';
+        $employeeType = 'engineer';
+        $status = 'active';
+        $workPhones = new PhoneCollection();
+        $mobilePhones = new PhoneCollection();
+        $rooms = new RoomCollection();
+        $department = '11111';
+        $urls = new LabelledUrlCollection();
+        $emailForwardings = new EmailAddressCollection();
+        $emailAlternatives = new EmailAddressCollection();
         
         $user = new User();
         $user->setId($id);
         $user->setUsername($username);
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setFullName($fullName);
+        $user->setEmail($email);
+        $user->setEmployeeType($employeeType);
+        $user->setStatus($status);
+        $user->setWorkPhones($workPhones);
+        $user->setMobilePhones($mobilePhones);
+        $user->setRooms($rooms);
+        $user->setDepartment($department);
+        $user->setUrls($urls);
+        $user->setEmailForwardings($emailForwardings);
+        $user->setEmailAlternatives($emailAlternatives);
         
         $this->assertSame($id, $user->getId());
         $this->assertSame($username, $user->getUsername());
+        $this->assertSame($firstName, $user->getFirstName());
+        $this->assertSame($lastName, $user->getLastName());
+        $this->assertSame($fullName, $user->getFullName());
+        $this->assertSame($email, $user->getEmail());
+        $this->assertSame($employeeType, $user->getEmployeeType());
+        $this->assertSame($status, $user->getStatus());
+        $this->assertSame($workPhones, $user->getWorkPhones());
+        $this->assertSame($mobilePhones, $user->getMobilePhones());
+        $this->assertSame($rooms, $user->getRooms());
+        $this->assertSame($department, $user->getDepartment());
+        $this->assertSame($urls, $user->getUrls());
+        $this->assertSame($emailForwardings, $user->getEmailForwardings());
+        $this->assertSame($emailAlternatives, $user->getEmailAlternatives());
+    }
+
+
+    public function testGetWorkPhonesWithImplicitValue()
+    {
+        $user = new User();
+        
+        $phonesCollection = $user->getWorkPhones();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\PhoneCollection', $phonesCollection);
+    }
+
+
+    public function testSetWorkPhonesWithArray()
+    {
+        $phones = array(
+            '123',
+            '456'
+        );
+        
+        $user = new User();
+        $user->setWorkPhones($phones);
+        
+        $phonesCollection = $user->getWorkPhones();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\PhoneCollection', $phonesCollection);
+        $this->assertSame('123', $phonesCollection->get(0)
+            ->getValue());
+        $this->assertSame('456', $phonesCollection->get(1)
+            ->getValue());
+    }
+
+
+    public function testGetMobilePhonesWithImplicitValue()
+    {
+        $user = new User();
+        
+        $phonesCollection = $user->getMobilePhones();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\PhoneCollection', $phonesCollection);
+    }
+
+
+    public function testSetMobilePhonesWithArray()
+    {
+        $phones = array(
+            '123',
+            '456'
+        );
+        
+        $user = new User();
+        $user->setMobilePhones($phones);
+        
+        $phonesCollection = $user->getMobilePhones();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\PhoneCollection', $phonesCollection);
+        $this->assertSame($phones, $phonesCollection->toPlainArray());
+    }
+
+
+    public function testGetUrlsWithImplicitValue()
+    {
+        $user = new User();
+        
+        $urls = $user->getUrls();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\LabelledUrlCollection', $urls);
+    }
+
+
+    public function testGetEmailForwardingsWithImplicitValue()
+    {
+        $user = new User();
+        
+        $emails = $user->getEmailForwardings();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\EmailAddressCollection', $emails);
+    }
+
+
+    public function testSetEmailForwardingsWithArray()
+    {
+        $emails = array(
+            'foo@bar',
+            'test@example'
+        );
+        
+        $user = new User();
+        $user->setEmailForwardings($emails);
+        
+        $emailsCollection = $user->getEmailForwardings();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\EmailAddressCollection', $emailsCollection);
+        $this->assertSame($emails, $emailsCollection->toPlainArray());
+    }
+
+
+    public function testGetEmailAlternativesWithImplicitValue()
+    {
+        $user = new User();
+        
+        $emails = $user->getEmailAlternatives();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\EmailAddressCollection', $emails);
+    }
+
+
+    public function testSetEmailAlternativesWithArray()
+    {
+        $emails = array(
+            'foo@bar',
+            'test@example'
+        );
+        
+        $user = new User();
+        $user->setEmailAlternatives($emails);
+        
+        $emailsCollection = $user->getEmailAlternatives();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\EmailAddressCollection', $emailsCollection);
+        $this->assertSame($emails, $emailsCollection->toPlainArray());
+    }
+
+
+    public function testGetRoomsWithImplicitValue()
+    {
+        $user = new User();
+        
+        $rooms = $user->getRooms();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\RoomCollection', $rooms);
+    }
+
+
+    public function testSetRoomsWithArray()
+    {
+        $rooms = array(
+            'A123',
+            'B456'
+        );
+        
+        $user = new User();
+        $user->setRooms($rooms);
+        
+        $roomsCollection = $user->getRooms();
+        $this->assertInstanceOf('Udb\Domain\Entity\Collection\RoomCollection', $roomsCollection);
+        $this->assertSame($rooms, $roomsCollection->toPlainArray());
     }
 }
