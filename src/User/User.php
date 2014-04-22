a36497a1e06fa432ce4fb27bbae040b1d5e5d915
Ivan Novakov <ivan.novakov@debug.cz>
2014-04-22 10:35:27 +0200

    user + hydrator fix

--- src/User/User.php
+++ src/User/User.php
@@ -105,7 +105,11 @@ class User
      */
     public function setId($id)
     {
-        $this->id = $id;
+        if (! is_scalar($id)) {
+            throw new InvalidValueException('The ID should be a scalar value (integer)');
+        }
+        
+        $this->id = intval($id);
     }
 
 
@@ -123,7 +127,7 @@ class User
      */
     public function setUsername($username)
     {
-        $this->username = $username;
+        $this->username = (string) $username;
     }
 
 
@@ -141,7 +145,7 @@ class User
      */
     public function setFirstName($firstName)
     {
-        $this->firstName = $firstName;
+        $this->firstName = (string) $firstName;
     }
 
 
@@ -159,7 +163,7 @@ class User
      */
     public function setLastName($lastName)
     {
-        $this->lastName = $lastName;
+        $this->lastName = (string) $lastName;
     }
 
 
@@ -177,7 +181,7 @@ class User
      */
     public function setFullName($fullName)
     {
-        $this->fullName = $fullName;
+        $this->fullName = (string) $fullName;
     }
 
 
@@ -195,7 +199,7 @@ class User
      */
     public function setEmail($email)
     {
-        $this->email = $email;
+        $this->email = (string) $email;
     }
 
 
@@ -213,7 +217,7 @@ class User
      */
     public function setEmployeeType($employeeType)
     {
-        $this->employeeType = $employeeType;
+        $this->employeeType = (string) $employeeType;
     }
 
 
@@ -231,7 +235,7 @@ class User
      */
     public function setStatus($status)
     {
-        $this->status = $status;
+        $this->status = (string) $status;
     }
 
 
@@ -330,7 +334,7 @@ class User
      */
     public function setDepartment($department)
     {
-        $this->department = $department;
+        $this->department = (string) $department;
     }
 
 
